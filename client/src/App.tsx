c3ed405759e6e06108aec71996b05e4d0247984f
Kemuel Germones <kemuelgermones@gmail.com>
2022-11-18 22:57:16 +0800

    Added projects layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { MdOutlineDashboardCustomize } from "react-icons/md";
 import { AiOutlineWarning } from "react-icons/ai";
 import { FiUsers } from "react-icons/fi";
 import { IoIosLogOut } from "react-icons/io";
+import { BsPlusLg } from "react-icons/bs";
 
 import SideNav from "./components/SideNav";
 import Container from "./components/Container";
@@ -28,7 +29,33 @@ function App() {
         </ul>
       </SideNav>
       <Container>
-        
+        <div>
+          <BsPlusLg size="1.5em" />
+          <h1>Add project</h1>
+        </div>
+        <div>
+          <h1>Project Two</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+            nostrum voluptas, et quod rem ipsam reiciendis.
+          </p>
+        </div>
+        <div>
+          <h1>Project One</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+            nostrum voluptas, et quod rem ipsam reiciendis facere quaerat quae
+            voluptatum autem molestiae numquam animi, veritatis nemo earum
+            reprehenderit doloribus voluptatem!
+          </p>
+        </div>
+        <div>
+          <h1>Project Two</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+            nostrum voluptas, et quod rem ipsam reiciendis.
+          </p>
+        </div>
       </Container>
     </Fragment>
   );
--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -2,10 +2,39 @@ import styled from "styled-components";
 
 const Container = styled.section`
   margin: 0 0 0 10rem;
-  height: 100vh;
-  display: flex;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 1rem;
+  grid-row-gap: 1rem;
+  padding: 1rem;
   box-sizing: border-box;
-  background-color: #bdc5f2;
+
+  & div {
+    background: #F2F2F2;
+    height: 15rem;
+    border-radius: 0.5rem;
+    box-shadow: 0 1px 25px rgba(0,0,0,0.2);
+    box-sizing: border-box;
+    padding: 1rem;
+  }
+
+  & h1 {
+    font-weight: bold;
+    font-size: 1.5rem;
+  }
+
+  & div:nth-child(1) {
+    color: blue;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  & div:nth-child(1) h1 {
+    margin-top: 1rem;
+  }
 `;
+
 
 export default Container;
